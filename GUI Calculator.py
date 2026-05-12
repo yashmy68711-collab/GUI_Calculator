e7a8a18add90a97b4a0e4d650739cade4987b605
Yash <yashmy68711@gmail.com>
2026-05-12 09:54:54 +0530

    Update GUI Calculator.py

--- GUI Calculator.py
+++ GUI Calculator.py
@@ -37,27 +37,41 @@ window = tk.Tk()
 window.title("GUI Calculator")
 window.geometry("300x250")
 
-# Inputs
-tk.Label(window, text="First Number").pack()
+tk.Label(window, text="First Number").pack(pady=5)
+
 entry1 = tk.Entry(window)
 entry1.pack()
 
-tk.Label(window, text="Second Number").pack()
+tk.Label(window, text="Second Number").pack(pady=5)
+
 entry2 = tk.Entry(window)
 entry2.pack()
 
-# Buttons
-tk.Button(window, text="Add", command=lambda: calculate("+")).pack(pady=5)
+tk.Button(window, text="Add", width=15,
+command=lambda: calculate("+")).pack(pady=5)
 
-tk.Button(window, text="Subtract", command=lambda: calculate("-")).pack(pady=5)
+tk.Button(window, text="Subtract", width=15,
+command=lambda: calculate("-")).pack(pady=5)
 
-tk.Button(window, text="Multiply", command=lambda: calculate("*")).pack(pady=5)
+tk.Button(window, text="Multiply", width=15,
+command=lambda: calculate("*")).pack(pady=5)
 
-tk.Button(window, text="Divide", command=lambda: calculate("/")).pack(pady=5)
+tk.Button(window, text="Divide", width=15,
+command=lambda: calculate("/")).pack(pady=5)
 
-# Output
-output = tk.Label(window, text="Result will appear here")
+tk.Button(window, text="Power", width=15,
+command=lambda: calculate("^")).pack(pady=5)
+
+tk.Button(window, text="Clear", width=15,
+command=clear).pack(pady=5)
+
+output = tk.Label(
+    window,
+    text="Result will appear here",
+    font=("Arial", 12)
+)
+
 output.pack(pady=10)
 
-# Run
+# Run App
 window.mainloop()
